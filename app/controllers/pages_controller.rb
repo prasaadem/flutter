6871371c51885c03ac03eb97da34fcb71cc70186
Aditya Emani <prasaadem@gmail.com>
2017-02-02 23:17:09 +0000

    Added UI, Post functionality, Followers, Trends

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
 
   def home
     @posts = Post.all
+    @newpost = Post.new
   end
 
   def profile
@@ -16,9 +17,13 @@ class PagesController < ApplicationController
     
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newpost = Post.new
+    
+    @toFollow = User.all.last(5)
   end
 
   def explore
+    @toFollow = User.all.last(5)
     @posts = Post.all
+    @newpost = Post.new
   end
 end
